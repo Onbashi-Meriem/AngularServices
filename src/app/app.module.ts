7f70bed272c78670472a8e8c28dd061e9ecbc6b7
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-17 15:32:56 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
